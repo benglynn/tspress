ccb2016b03a30adc4a1f381d13ee5317cf67da75
Ben Glynn <ben@benedictglynn.com>
2020-07-10 13:19:18 +0100

    Setup TypeSdript

---
+++ dist/mod.js
@@ -0,0 +1,4 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var message = "Hello from module";
+exports.default = message;
---
+++ src/index.ts
@@ -0,0 +1,3 @@
+import message from "./mod";
+
+console.log(message);
