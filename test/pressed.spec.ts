a1a78d7ad71b2dcb7669b6ddc1cbaf9926ea752c
Ben Glynn <ben@benedictglynn.com>
2020-07-15 14:43:09 +0100

    pipeables may be asynchronous

--- test/pressed.spec.ts
+++ test/pressed.spec.ts
@@ -51,7 +51,13 @@ describe("press", () => {
   });
 
   describe("asPipe", () => {
-    it("creates a pipe that transforms through pipeables", async () => {
+    it("returns a function", async () => {
+      expect(asPipe([], {})).to.be.a("function");
+    });
+  });
+
+  describe("pipe", () => {
+    it("passes pages through any number of pipeables", async () => {
       interface Context {
         names: Array<string>;
         pageCount: number;
@@ -74,6 +80,61 @@ describe("press", () => {
           ...page,
           extra: `Page ${index + 1} of ${context.pageCount}`,
         }));
+
+      const justExtra: P<Context, PageExtra[], string[]> = (pages) =>
+        pages.map((page) => page.extra);
+
+      const upNames = ["", "FRENCH-PRESS", "TEA-POT"];
+      const starNames = ["", "FRENCH*PRESS", "TEA*POT"];
+      const extra = ["Page 1 of 3", "Page 2 of 3", "Page 3 of 3"];
+
+      expect((await pipe(upper)).map((page) => page.name)).to.deep.equal(
+        upNames
+      );
+
+      expect(
+        (await pipe(upper, starDashes)).map((page) => page.name)
+      ).to.deep.equal(starNames);
+
+      expect(
+        (await pipe(upper, starDashes, addContext)).map((pg) => pg.extra)
+      ).to.deep.equal(extra);
+
+      expect(
+        await pipe(upper, starDashes, addContext, justExtra)
+      ).to.deep.equal(extra);
+    });
+
+    it("awaits promises returned by pipeables", async () => {
+      interface Context {
+        names: Array<string>;
+        pageCount: number;
+      }
+      interface PageExtra extends Page {
+        extra: string;
+      }
+      const { initial, reducers, fixturePath } = setup();
+      const [pages, context] = await press(fixturePath, initial, reducers);
+      const pipe = asPipe(pages, context);
+
+      const upper: P<Context, Page[], Promise<Page[]>> = (pages) =>
+        Promise.resolve(
+          pages.map((page) => ({ ...page, name: page.name.toUpperCase() }))
+        );
+
+      const starDashes: P<Context, Page[], Page[]> = (pages) =>
+        pages.map((page) => ({ ...page, name: page.name.replace(/-/g, "*") }));
+
+      const addContext: P<Context, Page[], Promise<PageExtra[]>> = (
+        pages,
+        context
+      ) =>
+        Promise.resolve(
+          pages.map((page, index) => ({
+            ...page,
+            extra: `Page ${index + 1} of ${context.pageCount}`,
+          }))
+        );
 
       const justExtra: P<Context, PageExtra[], string[]> = (pages) =>
         pages.map((page) => page.extra);
