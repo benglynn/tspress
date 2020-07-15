e3e7b2015a2b9a2e722d7a07ef5f46dd031364e3
Ben Glynn <ben@benedictglynn.com>
2020-07-15 14:59:15 +0100

    separated pipe type and added overloads

--- src/as-pipe.ts
+++ src/as-pipe.ts
@@ -1,16 +1,6 @@
 import Page from "./page";
 import Pipeable from "./pipeable";
-
-type P<T, A, B> = Pipeable<T, A, B>;
-type R<T> = T | Promise<T>;
-
-// prettier-ignore
-interface Pipe<T> {
-  <A>(p1: P<T, Page[], A>): Promise<A>;
-  <A, B>(p1: P<T, Page[], R<A>>, p2: P<T, A, B>): Promise<B>;
-  <A, B, C>(p1: P<T, Page[], R<A>>, p2: P<T, A, R<B>>, p3: P<T, B, C>): Promise<C>;
-  <A, B, C, D>(p1: P<T, Page[], R<A>>, p2: P<T, A, R<B>>, p3: P<T, B, R<C>>, p4: P<T, C, D>): Promise<D>;
-}
+import Pipe from "./pipe";
 
 const asPipe = <TContext>(pages: Page[], context: TContext): Pipe<TContext> => {
   return (...pipeables: Array<Pipeable<TContext, any, any>>): Promise<any> =>
