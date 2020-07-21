b098f4967bd5a5f1f8911d2b19662937f9025665
Ben Glynn <ben@benedictglynn.com>
2020-07-21 14:22:09 +0100

    Simplify tests & types (#4)
    * Remove explicit return type
    
    * Remove context pipeable type
    
    * pipeable type has context
    
    * better organised tests
    
    * removed unnecessary test setup

--- src/make-pipe.ts
+++ src/make-pipe.ts
@@ -1,11 +1,12 @@
 import Pipeable from "./types/pipeable";
 import Pipe from "./types/pipe";
 
-const makePipe = <TStart, TEnd, TContext>(): Pipe<TStart, TEnd, TContext> => (
-  ...pipeables: Array<Pipeable<any, any>>
+const makePipe = <TStart, TContext>(): Pipe<TStart, TContext> => (
+  ...pipeables: Array<Pipeable<any, any, TContext>>
 ) => (start: TStart, context: TContext) =>
   pipeables.reduce<any>(async (previous, pipe) => {
     const piped = pipe(await previous, context);
     return (piped instanceof Promise && piped) || Promise.resolve(piped);
   }, start);
+
 export default makePipe;
