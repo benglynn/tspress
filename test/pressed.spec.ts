1de31ef09662896a393b5ab27eeae7e6cad0ad32
Ben Glynn <ben@benglynn.com>
2020-07-21 17:27:01 +0100

    Tags (#5)
    * removed unnecessary item param
    
    * md-meta pipeable
    
    * md-meta spec
    
    * Added metadata to home
    
    * tag reducer

--- test/pressed.spec.ts
+++ test/pressed.spec.ts
@@ -3,15 +3,34 @@ import { expect } from "chai";
 import { join } from "path";
 import { press, makePipe } from "../src/api";
 import Page from "../src/types/page";
-import pressed from "./expected/pressed";
 
 describe("press", () => {
   interface PageExtra extends Page {
     extra: string;
   }
 
+  const expected = [
+    {
+      name: "",
+      path: "/",
+      md:
+        "---\ntemplate: 'home.pug'\ntags:\n - global\n---\n\n# Beverage vessels\n\nWelcome!",
+    },
+    {
+      name: "french-press",
+      path: "/french-press/",
+      md:
+        "---\nheadline: French press\ntags:\n - vessel\n---\n\n# French press\n\nThe French press is also known as a cafetière.",
+    },
+    {
+      name: "tea-pot",
+      path: "/tea-pot/",
+      md:
+        "---\nheadline: Tea pot\ntags:\n - vessel\n---\n\n# Tea pot\n\nA teapot is a vessel used for steeping tea leaves.",
+    },
+  ];
+
   const setup = () => {
-    const items: Page[] = [];
     const toItem = (page: Page) => page;
     const seed = { names: <string[]>[], pageCount: 0 };
     const reducers = {
@@ -19,25 +38,19 @@ describe("press", () => {
       pageCount: (page: Page, previous: number) => previous + 1,
     };
     const fixturePath = join(__dirname, "fixture");
-    return { items, toItem, seed, reducers, fixturePath };
+    return { toItem, seed, reducers, fixturePath };
   };
 
   it("should parse each directory", async () => {
     const { toItem, fixturePath } = setup();
-    const [pages] = await press(fixturePath, [], toItem, {}, {});
-    expect(pages).to.deep.equal(pressed);
+    const [pages] = await press(fixturePath, toItem, {}, {});
+    expect(pages).to.deep.equal(expected);
   });
 
   it("should fold context with reducers", async () => {
-    const { items, toItem, seed, reducers, fixturePath } = setup();
-    const [pages, context] = await press(
-      fixturePath,
-      items,
-      toItem,
-      seed,
-      reducers
-    );
-    expect(pages).to.deep.equal(pressed);
+    const { toItem, seed, reducers, fixturePath } = setup();
+    const [pages, context] = await press(fixturePath, toItem, seed, reducers);
+    expect(pages).to.deep.equal(expected);
     expect(context).to.deep.equal({
       names: ["", "french-press", "tea-pot"],
       pageCount: 3,
@@ -45,21 +58,15 @@ describe("press", () => {
   });
 
   it("should fold context with async reducers", async () => {
-    const { items, toItem, seed, fixturePath } = setup();
-    const asyncReducers = {
+    const { toItem, seed, fixturePath } = setup();
+    const reducers = {
       names: (page: Page, previous: string[]) =>
         Promise.resolve(previous.concat(page.name)),
       pageCount: (page: Page, previous: number) =>
         Promise.resolve(previous + 1),
     };
-    const [pages, context] = await press(
-      fixturePath,
-      items,
-      toItem,
-      seed,
-      asyncReducers
-    );
-    expect(pages).to.deep.equal(pressed);
+    const [pages, context] = await press(fixturePath, toItem, seed, reducers);
+    expect(pages).to.deep.equal(expected);
     expect(context).to.deep.equal({
       names: ["", "french-press", "tea-pot"],
       pageCount: 3,
@@ -69,7 +76,7 @@ describe("press", () => {
   it("transforms each page", async () => {
     const { fixturePath } = setup();
     const toItem = (page: Page): PageExtra => ({ ...page, extra: "sauce" });
-    const [pages] = await press(fixturePath, [], toItem, {}, {});
+    const [pages] = await press(fixturePath, toItem, {}, {});
     const expected = ["sauce", "sauce", "sauce"];
     expect(pages.map((page) => page.extra)).to.deep.equal(expected);
   });
@@ -78,7 +85,7 @@ describe("press", () => {
     const { fixturePath } = setup();
     const pipeable = (page: Page): PageExtra => ({ ...page, extra: "pipe" });
     const pipe = makePipe<Page, null>()(pipeable);
-    const [pages] = await press(fixturePath, [], pipe, {}, {});
+    const [pages] = await press(fixturePath, pipe, {}, {});
     const expected = ["pipe", "pipe", "pipe"];
     expect(pages.map((page) => page.extra)).to.deep.equal(expected);
   });
@@ -88,7 +95,7 @@ describe("press", () => {
     const pipeable = (page: Page): Promise<PageExtra> =>
       Promise.resolve({ ...page, extra: "async pipe" });
     const pipe = makePipe<Page, null>()(pipeable);
-    const [pages] = await press(fixturePath, [], pipe, {}, {});
+    const [pages] = await press(fixturePath, pipe, {}, {});
     const expected = ["async pipe", "async pipe", "async pipe"];
     expect(pages.map((page) => page.extra)).to.deep.equal(expected);
   });
