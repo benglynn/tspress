a5317c013be4f01763957451f5308d8082479930
Ben Glynn <ben@benedictglynn.com>
2020-07-22 14:33:48 +0100

    transform md html

---
+++ src/pipeables/md-html.ts
@@ -0,0 +1,20 @@
+import Page from "../types/page";
+import showdown from "showdown";
+import { PageMeta } from "./md-meta";
+
+interface PageHtml extends PageMeta {
+  mdHtml: string;
+}
+
+showdown.setFlavor("github");
+const converter = new showdown.Converter({
+  noHeaderId: true,
+  simpleLineBreaks: false,
+  metadata: true,
+});
+
+const mdHtml = (item: PageMeta): PageHtml => {
+  return { ...item, mdHtml: converter.makeHtml(item.md) };
+};
+
+export default mdHtml;
