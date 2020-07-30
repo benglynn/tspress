435ce8fe8be465d6a419899a84583177a23900e7
Ben Glynn <ben@benedictglynn.com>
2020-07-30 17:25:06 +0100

    Corrected pipe return type

--- src/pipe.ts
+++ src/pipe.ts
@@ -2,7 +2,7 @@ import Pipeable from "./types/pipeable";
 
 const pipe = <TInOut, TContext>(
   ...pipeables: ReadonlyArray<Pipeable<TInOut, TContext>>
-) => (start: TInOut, context: TContext): TInOut => {
+) => (start: TInOut, context: TContext): Promise<TInOut> => {
   return pipeables.reduce<any>(async (previous, pipeable) => {
     const piped = pipeable(await previous, context);
     return (piped instanceof Promise && piped) || Promise.resolve(piped);
