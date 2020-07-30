aaacb77b5e5d86039a78e44fc8ed30545e2bd307
Ben Glynn <ben@benedictglynn.com>
2020-07-30 17:25:33 +0100

    Render html assets

--- src/tap-async-callback.ts
+++ src/tap-async-callback.ts
@@ -4,20 +4,18 @@ import {
   toPage,
   seed as defaultSeed,
   reducers as defaultReducers,
+  render,
 } from "./defaults";
 import press from "./press";
 import { webpackReducer, webpackSeed } from "./reducers/webpack";
-import { tagsReducer } from "./reducers/tags";
 
 const tapAsyncCallback = (
   options: WebpackOptions,
   content: string, // absolute path to markdown directory
   templates: string // absolute path to pug templates
 ) => {
-  console.log("just once surely?");
   const fileTimes = new Map<string, number | undefined>();
   const seed = { ...defaultSeed, webpack: webpackSeed };
-
   return (compilation: WebpackCompilation, done: () => void) => {
     const reducers = {
       ...defaultReducers,
@@ -25,9 +23,19 @@ const tapAsyncCallback = (
     };
     press(content, templates, toPage, seed, reducers).then(
       ([pages, context]) => {
-        console.log("\n*************\ntspress\n");
-        console.log(`changes: ${context.webpack.changed.length}`);
-        done();
+        if (context.webpack.changed.length === 0) {
+          done();
+          return;
+        }
+        render(pages, context).then((pages) => {
+          pages.map((page) => {
+            compilation.assets[`${page.path}index.html`.substring(1)] = {
+              size: () => page.html.length,
+              source: () => page.html,
+            };
+          });
+          done();
+        });
       }
     );
   };
