fe7091defb628f523f1436b541cf53dc7639898b
Ben Glynn <ben@benglynn.com>
2020-08-05 09:17:44 +0100

    Covered double escaping in code blocks

--- src/render-pipe/highlight-code.ts
+++ src/render-pipe/highlight-code.ts
@@ -1,6 +1,10 @@
 import RenderPipeable from "../types/render-pipeable";
 import prism from "prismjs";
 
+const extraLangs = ["typescript"];
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+require("prismjs/components/")(extraLangs);
+
 const pattern = /<pre><code class="([-a-z]+) language-\1">([\s\S]*?)<\/code><\/pre>/g;
 
 const highlightCode: RenderPipeable = (page) => ({
@@ -10,7 +14,11 @@ const highlightCode: RenderPipeable = (page) => ({
     if (prismLang === undefined)
       throw new Error(`prism didn't recognise language '${langCode}'`);
     const css = `class="language-${langCode}"`;
-    const highlighted = prism.highlight(code, prismLang, langCode);
+    const unescaped = code
+      .replace(/&gt;/g, ">")
+      .replace(/&lt;/g, "<")
+      .replace(/&amp;/g, "&");
+    const highlighted = prism.highlight(unescaped, prismLang, langCode);
     return `<pre ${css}"><code ${css}>${highlighted}}</code></pre>`;
   }),
 });
---
+++ test/highlight-code.spec.ts
@@ -0,0 +1,36 @@
+import "mocha";
+import { expect } from "chai";
+import highlightCode from "../src/render-pipe/highlight-code";
+import Page from "../src/types/page";
+import RenderContext from "../src/types/render-context";
+
+const mdHtml = `<pre><code class="ts language-ts">type F = &lt;T&gt;(s: T) =&gt; T;
+const fun: F = (s) =&gt; s;
+const drink = (name === "Bob" &amp;&amp; "Coffee") || "Tea";</code></pre>`;
+
+const page: Page = {
+  dependencies: [],
+  html: "",
+  md: "",
+  mdHtml,
+  mdMeta: {},
+  name: "",
+  path: "",
+  template: "",
+};
+
+const expected =
+  "type F = &lt;T>(s: T) => T;\nconst fun: F = (s) => s;\n" +
+  'const drink = (name === "Bob" &amp;&amp; "Coffee") || "Tea";}';
+
+const context: RenderContext = {
+  tags: {},
+};
+
+describe("highlightCode", () => {
+  it("does not double escape entities", async () => {
+    const mdHtml = (await highlightCode(page, context)).mdHtml;
+    const stripTags = mdHtml.replace(/\<\/?([a-z]+)[^>]*\>/g, "");
+    expect(stripTags).to.equal(expected);
+  });
+});
